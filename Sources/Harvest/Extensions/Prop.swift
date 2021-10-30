75113bce8e7d4ea7decfe72cefedb43e0942ea0f
Zack Brown <zack@zackbrown.co.uk>
2021-10-30 12:13:39 +0100

    Remove unused file

--- Sources/Harvest/Extensions/Prop.swift
+++ Sources/Harvest/Extensions/Prop.swift
@@ -15,6 +15,7 @@ extension Prop {
         case .bridge: return Footprint(coordinate: .zero)
         case .building(_, let polyomino): return polyomino.footprint
         case .foliage: return Footprint(coordinate: .zero)
+        case .portal: return Footprint(coordinate: .zero)
         case .stairs: return Footprint(coordinate: .zero)
         case .wall: return Footprint(coordinate: .zero)
         }
@@ -27,6 +28,7 @@ extension Prop {
         case .bridge: return .bridges
         case .building: return .buildings
         case .foliage: return .foliage
+        case .portal: return .portals
         case .stairs: return .stairs
         case .wall: return .walls
         }
