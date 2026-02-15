24cec178b57ff195781b71fb9bc87f35a924a0e6
Zack Brown <zack@zackbrown.co.uk>
2026-02-15 12:27:57 +0000

    Implement portal system chunk meshing

--- Sources/Harvest/Entites/Portals/PortalSystem.swift
+++ Sources/Harvest/Entites/Portals/PortalSystem.swift
@@ -8,6 +8,7 @@
 import AppKit
 import Deltille
 import Euclid
+import Lattice
 import RealityKit
 
 @MainActor
@@ -24,7 +25,60 @@ internal struct PortalSystem: System {
             
             let terrainSlice = terrain.slice(for: chunk.triangle.sieve(for: .chunk))
             
-            return false
+            return update(grid: portals,
+                          chunk: chunk,
+                          slice: slice,
+                          terrainSlice: terrainSlice)
         }
+    }
+}
+
+extension PortalSystem {
+    
+    private func update(grid: Portals,
+                        chunk: PortalChunk,
+                        slice: TriangularGridDataSourceSlice<PortalTile>,
+                        terrainSlice: HexagonalGridDataSourceSlice<TerrainVertex>) -> Bool {
+        
+        var invalidTiles: [Triangle] = []
+        
+        let polygons = slice.tiles.reduce(into: [Euclid.Polygon]()) { result, tile in
+            
+            guard let terrainTile = terrainSlice.tile(for: tile.triangle),
+                  let elevation = terrainTile.uniformElevation else {
+                
+                invalidTiles.append(tile.triangle)
+                
+                return
+            }
+            
+            result.append(contentsOf: render(tile: terrainTile,
+                                             elevation: elevation))
+        }
+        
+        grid.remove(values: invalidTiles)
+        
+        guard !polygons.isEmpty else { return false }
+        
+        let mesh = Mesh(polygons)
+        
+        chunk.mesh = mesh.translated(by: -chunk.triangle.position(chunk.scale))
+        
+        return true
+    }
+    
+    private func render(tile: HexagonalGridDataSourceTile<TerrainVertex>,
+                        elevation: Int) -> [Euclid.Polygon] {
+        
+        let apex = Vector(0.0,
+                          TerrainSystem.unitHeight(for: elevation) + 0.01,
+                          0.0)
+        
+        let vertices = tile.vertices.map { $0.key.position(.tile) + apex }
+        
+        let volume = Volume(vertices: vertices,
+                            displacement: Triangle.Scale.tile.edgeLength / 2.0)
+        
+        return volume.mesh(.red).polygons
     }
 }
